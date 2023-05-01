2cb2fdd55e8b2eabaee99b1458af96013be6f7f7
amaan-ahmad <amaanahmad582001@gmail.com>
2023-05-01 13:44:55 +0530

    Stage 2 passed

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strings"
@@ -18,29 +19,50 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error accepting connection")
 		os.Exit(1)
 	}
-	defer l.Close()
+	buff := make([]byte, 50)
+	c := bufio.NewReader(conn)
+
 	for {
-		println("Waiting for message...")
-		netData, err := bufio.NewReader(conn).ReadString('\n')
+		foundCommand := false
+		// read the full message, or return an error
+		n, err := c.Read(buff)
 		if err != nil {
-			fmt.Println(err)
-			return
+			if err == io.EOF {
+				fmt.Println("Connection closed")
+				os.Exit(0)
+			}
+			fmt.Println("Error reading from connection")
+			os.Exit(1)
 		}
-		input := strings.TrimSpace(string(netData))
-		if strings.TrimSpace(input) == "stop" {
-			fmt.Println("Exiting TCP server!")
-			return
+
+		// split the buffer into lines and print them
+		s := string(buff[:n])
+		fmt.Println("Received: ", s)
+		for _, line := range strings.Split(s, "\r\n") {
+			line = strings.ToUpper(strings.TrimSpace(line))
+			if line == "PING" {
+				foundCommand = true
+				clearBuffer(buff)
+				conn.Write([]byte("+PONG\r\n"))
+				break
+			}
 		}
-		fmt.Println("-> ", input)
-		if input == "PING" {
-			conn.Write([]byte("+PONG\r\n"))
-		} else {
-			conn.Write([]byte("-ERR unknown command '" + input + "'\r\n"))
+		if foundCommand {
+			continue
 		}
+		clearBuffer(buff)
+		conn.Write([]byte("-ERR unknown command\r\n"))
+	}
+}
+
+func clearBuffer(buff []byte) {
+	for i := range buff {
+		buff[i] = 0
 	}
 }
